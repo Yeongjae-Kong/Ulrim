cc2934182a83af335a777760ad853abf228ca590
Yeongjae Kong <kong7300@dgist.ac.kr>
2021-11-18 13:32:16 +0900

    add

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,21 @@
+export type AmplifyDependentResourcesAttributes = {
+    "api": {
+        "amplifytest": {
+            "GraphQLAPIIdOutput": "string",
+            "GraphQLAPIEndpointOutput": "string"
+        }
+    },
+    "auth": {
+        "amplifytest620bf5b2": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "HostedUIDomain": "string",
+            "OAuthMetadata": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
